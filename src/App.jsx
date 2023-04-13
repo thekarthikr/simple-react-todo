9fd3304aa0c4740639280c37cbd3b5f0c568af84
Karthik <karthikwebdev@outlook.com>
2023-04-13 14:57:51 +0530

    add sweetalert2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import AddTodo from "./AddTodo";
 import TodoList from "./TodoList";
+import "./App.scss";
+import Swal from "sweetalert2";
 
 function App() {
   const [todos, setTodos] = useState(() => {
@@ -39,16 +41,30 @@ function App() {
   };
 
   const deleteTodo = (id) => {
-    setTodos((currentTodos) => {
-      return currentTodos.filter((todo) => todo.id !== id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setTodos((currentTodos) => {
+          return currentTodos.filter((todo) => todo.id !== id);
+        });
+        Swal.fire("Deleted!", "Your task has been deleted.", "success");
+      }
     });
   };
   return (
-    <>
-      <AddTodo onSubmit={addTodo} />
+    <div className='container'>
       <h1 className='todo__title'>Todo List</h1>
+      <AddTodo onSubmit={addTodo} />
+
       <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
-    </>
+    </div>
   );
 }
 
